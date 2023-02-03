41e7bc1eec2822b37aa2313078e70a20a83e603a
eizinxaid <valentyn.ivankov@gmail.com>
2023-02-03 23:15:27 +0200

    Added get request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'TestWebApp';
+  resultMessage: string = "";
+  constructor(private http: HttpClient) {}
+
+  testConnection(){
+    this.http.get("http://localhost:5209/WeatherForecast", {responseType: 'text'})
+      .subscribe(
+      data => {
+        this.resultMessage = data;
+      }
+    );
+  }
 }
